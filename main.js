4cca3cd177435f264e2a38c25c9dfdeaee532fbd
LuisAguadoVicaria <luisaguadovicaria@gmail.com>
2022-07-04 17:17:13 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+const getColor = (cardcode) => {
+	switch(Math.floor(Math.random() * 4) + 1) { // 1-4 rand
+		case 1:
+			return [cardcode, "black", "&spades;"]
+		case 2:
+			return [cardcode, "black", "&clubs;"]
+		case 3:
+			return [cardcode, "red", "&#9830;"]
+		case 4:
+			return [cardcode, "red", "&hearts;"]
+	}
+}
+const getCard = () => {
+	let num = Math.floor(Math.random() * 13) + 1 // 1-13 rand
+	switch(num) {
+		case 1:
+			return getColor("A")
+		case 11:
+			return getColor("J")
+		case 12:
+			return getColor("Q")
+		case 13:
+			return getColor("K")
+		default:
+			return getColor(num)
+	}
+}
+const loadCard = () => {
+	let card = getCard()
+	document.querySelector("figcaption").innerHTML = card[0]
+	Array.from(document.querySelectorAll("aside")).forEach((item) => {
+		item.style.color = card[1]
+		item.innerHTML = card[2]
+	})
+}
+const handleInput = (event) => {
+	let input = parseInt(event.target.value)
+	isNaN(input) ? event.target.value = "Error Input" : document.querySelector("article").style.width = input + 'px'
+}
+window.addEventListener("load", loadCard)
+document.querySelector("button").addEventListener("click", loadCard)
+document.querySelector("input").addEventListener("keyup", handleInput)
